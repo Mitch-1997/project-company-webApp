4524bfa70b577c51eeadfbb2507bcfa916335b4a
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-10-07 11:24:35 +0300

    feat(json): creates json

--- src/component/Services.js
+++ src/component/Services.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../Css/Service.css";
 import Card from "./Combined"
 
+
 const Services = () => {
+  const [services, setServices] = useState([])
+
+  
+  useEffect(() => {
+    fetch(`http://localhost:3000/services`)
+    .then((resp) => resp.json())
+    .then((data) =>(data.services))
+
+  }, []) 
+  console.log(services);
   return (
     <>
       <h1>Our Services</h1>
