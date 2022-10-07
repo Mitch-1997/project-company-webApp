890682de73c9b0a9d2507b5c2ffb7465696c8a0f
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-10-07 16:36:23 +0300

    feat(Service.js): making post request

--- src/component/Services.js
+++ src/component/Services.js
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from "react";
 import "../Css/Service.css";
 import Card from "./Combined"
+import { NavLink } from "react-router-dom";
+import { SettingsRemote } from "@mui/icons-material";
 
 
 const Services = () => {
   const [services, setServices] = useState([]);
- 
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
   
   
   useEffect(() => {
@@ -17,15 +21,61 @@ const Services = () => {
     });
 
   }, []); 
+
+  function handleSubmit(e){
+    fetch(`http://localhost:3000/services`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description,
+        image: image,
+      }),
+    });
+    alert("added successfully")
+  }
+
  
   return (
     <>
       <h1>Our Services</h1>
       <div className="solution">
-        {services.map((service)=>{
-          return (<Card service={service}/>)
+        {services.map((service) => {
+          return <Card service={service} />;
         })}
       </div>
+      <form onSubmit={handleSubmit} className="service form">
+        <div className="inline fields">
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            name="title"
+            placeholder="Title"
+          />
+          <input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+            name="description"
+            placeholder="Description"
+          />
+          <input
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            type="link"
+            name="image"
+            placeholder="Image"
+          />
+        </div>
+        <div className="btnBox">
+          <button className="btn" type="submit">
+            Add product
+          </button>
+        </div>
+      </form>
     </>
   );
 };
